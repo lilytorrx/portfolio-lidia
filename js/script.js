7efb11b5a31510efc042fee8b79920e1c454b7d9
lilytorrx <repositoriolidia@gmail.com>
2025-03-19 22:50:08 -0300

    feat: add no localStorage a preferência do usuário

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
 function changeMode() {
     const themeLink = document.getElementById('theme-link');
-    themeLink.href = themeLink.href.includes('dark-mode.css') ? '../css/light-mode.css' : '../css/dark-mode.css';
-    
-    let iconTheme = document.getElementById('icon-theme');
-    localStorage.setItem(themeLink, iconTheme)
-    
+    const iconTheme = document.getElementById('icon-theme');
+    const iconTheme2 = document.getElementsByClassName(".icon-theme")
+
     if (themeLink.href.includes('dark-mode.css')) {
-        iconTheme.src = '../img/light-mode.png';
-    } else {
+        themeLink.href = '../css/light-mode.css';
         iconTheme.src = '../img/dark-mode.png';
+        iconTheme2.src = '../img/dark-mode.png';
+        localStorage.setItem('theme', 'light');
+
+    } else {
+        themeLink.href = '../css/dark-mode.css';
+        iconTheme.src = '../img/light-mode.png';
+        iconTheme2.src = '../img/light-mode.png';
+        localStorage.setItem('theme', 'dark');
     }
 
-    console.log(themeLink.href, iconTheme.src);
+    console.log('Theme:', localStorage.getItem('theme'));
 }
 
-document.body.addEventListener("onload", () => {
-    console.log(localStorage.getItem(themeLink, iconTheme))
-})
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'dark';
+    const themeLink = document.getElementById('theme-link');
+    const iconTheme = document.getElementById('icon-theme');
+    const iconTheme2 = document.getElementsByClassName(".icon-theme")
+
+    if (savedTheme === 'light') {
+        themeLink.href = '../css/light-mode.css';
+        iconTheme.src = '../img/dark-mode.png';
+        iconTheme2.src = '../img/dark-mode.png';
+    } else {
+        themeLink.href = '../css/dark-mode.css';
+        iconTheme.src = '../img/light-mode.png';
+        iconTheme2.src = '../img/light-mode.png';
+    }
+
+    console.log(iconTheme2.src)
+    console.log('Tema:', savedTheme);
+});
