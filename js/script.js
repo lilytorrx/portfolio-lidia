c57542fd3a72593a36dc688464881e301ab7aacd
lilytorrx <repositoriolidia@gmail.com>
2025-03-19 23:22:35 -0300

    feat: add suavização ao mudar de tema

--- js/script.js
+++ js/script.js
@@ -1,40 +1,48 @@
 function changeMode() {
     const themeLink = document.getElementById('theme-link');
     const iconTheme = document.getElementById('icon-theme');
-    const iconTheme2 = document.getElementsByClassName(".icon-theme")
 
     if (themeLink.href.includes('dark-mode.css')) {
         themeLink.href = '../css/light-mode.css';
         iconTheme.src = '../img/dark-mode.png';
-        iconTheme2.src = '../img/dark-mode.png';
         localStorage.setItem('theme', 'light');
-
     } else {
         themeLink.href = '../css/dark-mode.css';
         iconTheme.src = '../img/light-mode.png';
-        iconTheme2.src = '../img/light-mode.png';
         localStorage.setItem('theme', 'dark');
     }
-
-    console.log('Theme:', localStorage.getItem('theme'));
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme') || 'dark';
     const themeLink = document.getElementById('theme-link');
     const iconTheme = document.getElementById('icon-theme');
-    const iconTheme2 = document.getElementsByClassName(".icon-theme")
 
     if (savedTheme === 'light') {
         themeLink.href = '../css/light-mode.css';
         iconTheme.src = '../img/dark-mode.png';
-        iconTheme2.src = '../img/dark-mode.png';
     } else {
         themeLink.href = '../css/dark-mode.css';
         iconTheme.src = '../img/light-mode.png';
-        iconTheme2.src = '../img/light-mode.png';
     }
-
-    console.log(iconTheme2.src)
-    console.log('Tema:', savedTheme);
 });
+
+$(document).ready(function() {
+    // Esconde os elementos inicialmente
+    $('.fade-in').css('opacity', 0);
+
+    $(window).on('scroll', function() {
+        $('.fade-in').each(function() {
+            var elementOffset = $(this).offset().top; // Posição do elemento
+            var windowScroll = $(window).scrollTop() + $(window).height(); // Posição do scroll
+
+            // Se o elemento estiver na tela
+            if (windowScroll > elementOffset) {
+                $(this).animate({
+                    opacity: 1
+                }, 800); // Animação para mostrar o elemento
+            }
+        });
+    });
+});
+
