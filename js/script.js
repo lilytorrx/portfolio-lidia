cd3d923a9c929c73600a8c1fa98ace03d86e682b
lilytorrx <repositoriolidia@gmail.com>
2025-03-19 22:15:16 -0300

    feat: add função que muda o tema pra escuro/claro

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 function changeMode() {
-
-}
-
-function changeLanguage() {
+    const themeLink = document.getElementById('theme-link');
+    themeLink.href = themeLink.href.includes('dark-mode.css') ? '../css/light-mode.css' : '../css/dark-mode.css';
     
+    let iconTheme = document.getElementById('icon-theme');
+    localStorage.setItem(themeLink, iconTheme)
+    
+    if (themeLink.href.includes('dark-mode.css')) {
+        iconTheme.src = '../img/light-mode.png';
+    } else {
+        iconTheme.src = '../img/dark-mode.png';
+    }
+
+    console.log(themeLink.href, iconTheme.src);
 }
+
+document.body.addEventListener("onload", () => {
+    console.log(localStorage.getItem(themeLink, iconTheme))
+})
