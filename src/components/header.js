09cbb2e3701971f1e52cfebce903382ef90e7d44
Lídia Torres <109477475+lilytorrx@users.noreply.github.com>
2025-07-27 13:50:26 -0300

    Começando as media queries

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,47 @@
 import { useState } from 'react';
 import logo from '../assets/imagotipo_deitado.png';
-
 import { Link } from 'react-router-dom';
 
 import '../css/header.css';
-import useScrollReveal from '../hooks/useScrollReveal';
 import Modal from './modal';
+import useScrollReveal from '../hooks/useScrollReveal';
+
+import { FiMenu, FiX } from 'react-icons/fi';
 
 function Header() {
-    
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
-    
     useScrollReveal(['.logo', 'nav'], { origin: 'top', delay: 100 });
 
     return (
         <header>
-            <figure className='logo'>
-                <img alt="Logo lilytorrx" src={logo} />
-            </figure>
-            <nav>
+            <div className='header-container'>
+                <figure className="logo">
+                    <img alt="Logo lilytorrx" src={logo} />
+                </figure>
+            
+                <button className="hamburger-icon" onClick={toggleMenu} aria-label="Abrir menu">
+                    {isMenuOpen ? <FiX size={30} color="#fff" /> : <FiMenu size={30} color="#fff" />}
+                </button>
+            </div>
+
+            <nav className={isMenuOpen ? 'open' : ''}>
                 <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/projects'>Projetos</Link></li>
-                    <li><Link to='/contact'>Contato</Link></li>
+                    <li><Link to='/' onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+                    <li><Link to='/projects' onClick={() => setIsMenuOpen(false)}>Projetos</Link></li>
+                    <li><Link to='/contact' onClick={() => setIsMenuOpen(false)}>Contato</Link></li>
                     <li>
-                        <button onClick={openModal} className="resume">Baixar currículo</button>
+                        <button 
+                            onClick={() => { openModal(); setIsMenuOpen(false); }} 
+                            className="resume"
+                        >
+                            Baixar currículo
+                        </button>
                     </li>
                 </ul>
             </nav>
